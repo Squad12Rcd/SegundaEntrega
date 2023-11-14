672f4a9fcb1e50c5819160034da801a596aa62e7
thallesSampaio <thallessampaio89@gmail.com>
2023-11-13 21:29:54 -0300

    first commit

---
+++ connectjob/src/main/java/com/connectjob/model/Empresa.java
@@ -0,0 +1,86 @@
+package com.connectjob.model;
+
+import java.util.List;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "empresa")
+public class Empresa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 80)
+	private String nome;	
+	
+	@Column(nullable = false, length = 18, unique = true)
+	private String cnpj;
+	
+	@Column(nullable = false, length = 32)
+	private String senha;
+	
+	@Column(nullable = false, length = 80, unique = true)
+    private String email;
+	
+	@OneToMany(mappedBy = "empresa")
+	private List<Vaga> vagas;
+	
+	public Empresa(Long id, String nome, String cnpj, String email, String senha) {
+		this.id = id;
+		this.nome = nome;
+		this.cnpj = cnpj;
+		this.email = email;
+		this.senha = senha;
+	}
+	
+	public Empresa() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
